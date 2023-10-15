f01118c99a5ff4e92436fae2ee0ea26a9019da57
Juan Pablo Gonzalez <jupago14@gmail.com>
2023-10-15 11:25:59 -0500

    Solving JWT issues

--- AeroApp/src/main/java/com/aeroapp/aeroapp/Security/Auth/AuthController.java
+++ AeroApp/src/main/java/com/aeroapp/aeroapp/Security/Auth/AuthController.java
@@ -2,8 +2,12 @@ package com.aeroapp.aeroapp.Security.Auth;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +22,11 @@ public class AuthController {
     @Autowired
     private final AuthService authService;
 
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .build();
+    }
     @Autowired
     private AuthenticationManager authenticationManager;
 
