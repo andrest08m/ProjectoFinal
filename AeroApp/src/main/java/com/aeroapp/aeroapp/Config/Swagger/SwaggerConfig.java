a53704d0cc06971e51f3d33857f8d63545a37d08
andrest08m <andressebastiantorresmesa@gmail.com>
2023-10-15 17:04:16 -0500

    swagger

---
+++ AeroApp/src/main/java/com/aeroapp/aeroapp/Config/Swagger/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.aeroapp.aeroapp.Config.Swagger;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)).paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        Contact[] contacts = new Contact[] {
+                new Contact("Daniel Espinosa", "https://www.linkedin.com/in/danessti/", "danessti@gmail.com"),
+                new Contact("Jorge Gomez", "https://www.linkedin.com/in/jorge-enrique-g%C3%B3mez-buitrago-31b083278/", "jorgegomez98125@gmail.com")
+        };
+
+        return new ApiInfoBuilder().title("API para gestión de Sistema de Vuelos")
+                .description("Esta API RESTful permite gestionar el Sistema de Reservas de Vuelos proporciona servicios para buscar vuelos, " +
+                        "realizar reservas, gestionar usuarios y cooperar con sistemas de aerolíneas.")
+                .version("1.0.0")
+                .contact(contacts[0])
+                .build();
+    }
+
+}
---
+++ AeroApp/src/main/java/com/aeroapp/aeroapp/Config/WebSecurityConfig.java
@@ -0,0 +1,92 @@
+package com.aeroapp.aeroapp.Config;
+
+import com.aeroapp.aeroapp.Security.Jwt.JwtAuthenticationFilter;
+import com.aeroapp.aeroapp.Security.Jwt.JwtAuthorizationFilter;
+import com.aeroapp.aeroapp.Security.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableConfigurationProperties
+@RequiredArgsConstructor
+@Profile("!test")
+public class WebSecurityConfig {
+
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity httpSecurity,
+                                            AuthenticationManager authManager) throws Exception {
+
+        JwtAuthenticationFilter jwtAuthenticationFilter =new JwtAuthenticationFilter();
+
+        jwtAuthenticationFilter.setAuthenticationManager(authManager);
+        jwtAuthenticationFilter.setFilterProcessesUrl("/login");
+
+        return httpSecurity
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/auth/**").permitAll()
+                .antMatchers("/v2/api-docs", "/configuration/ui",
+                        "/swagger-resources/**", "/configuration/security",
+                        "/swagger-ui.html", "/webjars/**", "/swagger-ui/**").permitAll()
+                .antMatchers("/v1/public").hasRole("USER")
+                .antMatchers("/v1/admin").hasRole("ADMIN")
+                /*.antMatchers(HttpMethod.GET).hasAuthority("READ")
+                .antMatchers(HttpMethod.POST).hasAuthority("WRITE")
+                .antMatchers(HttpMethod.DELETE).hasAuthority("DELETE")*/
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(jwtAuthenticationFilter)
+                .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsServiceImpl();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService())
+                .passwordEncoder(passwordEncoder())
+                .and()
+                .build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider= new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+}
